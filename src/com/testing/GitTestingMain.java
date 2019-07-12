4e4130792bbef5a7d32c0b7336023a6c3620e52b
zhangxiaoxiang <zhangxiaoxiang@10.0.7.120>
2019-07-12 12:17:39 +0800

    update project

---
+++ src/com/testing/GitTestingMain.java
@@ -0,0 +1,10 @@
+package com.testing;
+
+public class GitTestingMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a testing project!");
+	}
+
+}
